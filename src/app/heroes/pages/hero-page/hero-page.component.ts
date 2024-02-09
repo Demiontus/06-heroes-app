05085927999ffe5c84eacca6c301e422b0b0ba38
Demiontus <Brainloop.neskar@gmail.com>
2024-02-09 10:05:36 +0100

    new

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
 
+  public hero?: Hero;
+
+  constructor(private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        // delay(800),
+        switchMap(({ id }) => this.heroesService.getHeroId(id)),
+      ).subscribe(hero => {
+        if (!hero) return this.router.navigate(['/heroes/list']);
+        this.hero = hero;
+        return;
+      })
+
+  }
+  goBack():void{
+    this.router.navigateByUrl('heroes/list');
+  }
 }
--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
 })
 export class SearchPageComponent {
+
+
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+
+
+  constructor(private heroesService: HeroesService) { }
+
+
+  searchHero() {
+    const value: string = this.searchInput.value || "";
+    this.heroesService.getSuggestions(value)
+      .subscribe(heroes => this.heroes = heroes);
+
+      console.log(this.heroes);
+  }
 
 }
